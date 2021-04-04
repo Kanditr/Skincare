2db3eeff9b350a1f60b0a66b7a6ed596b626349d
Kanditr <gun.rodwong@gmail.com>
2021-04-04 20:45:58 +0700

    push

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { makeSelectUsers } from "./selectors";
+import Axios from "axios";
+
+const stateSelector = createSelector(makeSelectUsers, (users) => ({
+  users,
+}));
 
 export function HomePage(props) {
+  // eslint-disable-next-line
+  const { users } = useSelector(stateSelector);
+
+  const fetchUsers = async () => {
+    const response = await Axios.get("http://reqres.in/api/users?page=2").catch(
+      (err) => {
+        console.log("Err: ", err);
+      }
+    );
+
+    console.log("Users: ", response.data.data);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
   return (
     <div>
-      <h2>Hello Threena</h2>
+      <h2>Hello World</h2>
     </div>
   );
 }
