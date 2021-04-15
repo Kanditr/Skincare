8a708869dded99aa01e72c0d41370aaa71828d4e
Kanditr <gun.rodwong@gmail.com>
2021-04-15 21:49:04 +0700

    pushSync

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import "./App.css";
 import { AuthProvider } from "./auth";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./containers/homepage";
+import Bugs from "./components/Bugs";
+import configureStore from "./store/configureStore";
+import { Provider } from "react-redux";
+import BugsList from "./components/BugsList";
+
+const store = configureStore();
 
 function App() {
   return (
     <AuthProvider>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Homepage} />
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/1" component={Bugs} />
+            <Route exact path="/2" component={BugsList} />
+          </Switch>
+        </Router>
+      </Provider>
     </AuthProvider>
   );
 }
